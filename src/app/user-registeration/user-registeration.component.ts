627b9fa41c9f9ec8f57a70dde95b675b5c806413
soumya <soumyathamke@gmail.com>
2021-05-17 14:16:19 +0530

    first commit

---
+++ src/app/user-registeration/user-registeration.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-user-registeration',
+  templateUrl: './user-registeration.component.html',
+  styleUrls: ['./user-registeration.component.css']
+})
+export class UserRegisterationComponent {
+  
+
+}
